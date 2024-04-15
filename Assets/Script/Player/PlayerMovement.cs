a85d408190bc5988cfce95785578b4dcb0bc0f9a
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-04-15 12:05:13 +0200

    C

---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
