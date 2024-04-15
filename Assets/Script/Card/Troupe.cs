94a5d84e57ac1fc67e7f39b1a10d8254cd18ed14
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-04-15 19:20:17 +0200

    Le state est posé, on continue

--- Assets/Script/Card/Troupe.cs
+++ Assets/Script/Card/Troupe.cs
@@ -1,24 +1,3 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Troupe : Card
-{
-    [SerializeField] protected float moveSpd, range;
-
-    void Start()
-    {
-        type = TYPE.TROUPE;
-    }
-
-    void Update()
-    {
-        
-    }
-
-    public override void Invoque(Vector3 spawnPos)
-    {
-        GameObject objet =  Instantiate(gameObject);
-        objet.transform.position = spawnPos;
-    }
-}
--- Assets/Script/Ui/CardSelectionUI.cs
+++ Assets/Script/Ui/CardSelectionUI.cs
@@ -7,7 +7,9 @@ public class CardSelectionUI : MonoBehaviour
 {
     [SerializeField] private Deck deck;
     [SerializeField] private Transform C1, C2, C3, nextCard;
-    [SerializeField] private Image visuC1, visuC2, visuC3, visuNextCard;
+    [SerializeField] private Image visuC1, visuC2, visuC3, visuNextCard, cadre1, cadre2, cadre3;
+    [SerializeField] Color basicColor, selectedColor;
+    int selectedCard = 2;
     void Start()
     {
         deck.playACardEvent.AddListener(() => CheckVisu());
@@ -25,5 +27,15 @@ public class CardSelectionUI : MonoBehaviour
         visuC2 = Instantiate(deck.currentCards[1].visuUi, C2.transform);
         visuC3 = Instantiate(deck.currentCards[2].visuUi, C3.transform);
         visuNextCard = Instantiate(deck.nextCard.visuUi, nextCard.transform);
+        SetCadre();
+    }
+
+    private void SetCadre()
+    {
+        cadre1.color = basicColor;
+        cadre2.color = basicColor;
+        cadre3.color = basicColor;
+
+        Image cadreToChange = selectedCard == 1 ? cadre1 : selectedCard == 2 ? cadre2 : cadre3;
     }
 }
