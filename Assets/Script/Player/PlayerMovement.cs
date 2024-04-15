1d23bc1d2cb700723a488f857ee34c4ed0a6642d
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-04-15 13:12:51 +0200

    les controls marchent

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -3,28 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : GetInput
 {
-    private Rigidbody rb;
+    private Rigidbody2D rb;
+    [SerializeField] float moveSpd = 2f;
     protected override void Awake()
     {
         base.Awake();
         InstantiateAll();
     }
-    void Start()
-    {
-        
-    }
 
     private void Update()
     {
-        rb.velocity = direction * 10f;
+        rb.velocity = direction * moveSpd;
     }
-    
 
     private void InstantiateAll()
     {
-        rb = GetComponent<Rigidbody>(); 
+        rb = GetComponent<Rigidbody2D>(); 
     }
 }
