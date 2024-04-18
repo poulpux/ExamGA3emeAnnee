6d7ec26fdbb898de73a4ab361c81645d018955fe
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-04-18 09:18:38 +0200

    Le champiti marche

--- Assets/Script/Card/Sort/TirProjoChampi.cs
+++ Assets/Script/Card/Sort/TirProjoChampi.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class TirProjoChampi : MonoBehaviour
 { 
     [SerializeField] int damage;
+    [SerializeField] GameObject particles;
     private void OnDestroy()
     {
+        GameObject obj = Instantiate(particles.gameObject);
+        obj.transform.position = transform.position;    
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.45f, !GetComponent<Card>().J1 ? (1 << LayerMask.NameToLayer("TroupeP1")) : (1 << LayerMask.NameToLayer("TroupeP2")));
         foreach (var item in colliders)
         {
