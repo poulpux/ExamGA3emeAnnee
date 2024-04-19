01d6d10062f70405022576477e07dca83e3a2c9d
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-04-19 08:40:45 +0200

    Update CardChoice.cs

--- Assets/Script/Ui/CardChoice.cs
+++ Assets/Script/Ui/CardChoice.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -14,8 +15,10 @@ public class CardChoice : GetInput
     [HideInInspector] public int selectedCard = 5;
     [SerializeField] Color basicColor, selectedColor, cursorColor;
     [SerializeField] TextMeshProUGUI textReady;
+    private Color saveSelectedColor;
     void Start()
     {
+        saveSelectedColor = selectedColor;
         selectedCard = 5;
         isSwitchLeftEvent.AddListener(() => { selectedCard = selectedCard - 1 < 0 ? 12 : selectedCard - 1; SetCadre(); SwitchSelectEvent.Invoke(selectedCard); });
         isSwitchRightEvent.AddListener(() => { selectedCard = selectedCard + 1 > 12 ? 0 : selectedCard + 1; SetCadre(); SwitchSelectEvent.Invoke(selectedCard); });
@@ -28,6 +31,7 @@ public class CardChoice : GetInput
 
     private void SaveNumber()
     {
+        StopCoroutine(StartColorLerpSelect(true));
         bool delete = false;
         for(int i = 0; i < listSave.Count; i++)
         {
@@ -42,6 +46,7 @@ public class CardChoice : GetInput
             listSave.Add(selectedCard);
 
         SetReady();
+        StartCoroutine(StartColorLerpSelect(delete));
     }
 
     private void SetCadre()
@@ -53,8 +58,8 @@ public class CardChoice : GetInput
 
         foreach (var item in listSave)
         {
-            listCadre[item * 2].color = selectedColor;
-            listCadre[item * 2 + 1].color = selectedColor;
+            listCadre[item * 2].color = saveSelectedColor;
+            listCadre[item * 2 + 1].color = saveSelectedColor;
         }
 
         listCadre[selectedCard * 2].color = cursorColor;
@@ -65,4 +70,38 @@ public class CardChoice : GetInput
     {
             textReady.enabled = listSave.Count == 6 ? true : false;
     }
+
+    public float lerpDuration = 0.3f;
+
+
+    IEnumerator StartColorLerpSelect(bool Select)
+    {
+        float timeElapsed = 0f;
+        Color startColor = cursorColor;
+        Color endColor = !Select ? Color.white : basicColor;
+        Color currentColor = startColor;
+        int counter = 0;
+        while (counter <2)
+        {
+            while (timeElapsed < lerpDuration)
+            {
+                timeElapsed += Time.deltaTime;
+                float t = timeElapsed / lerpDuration;
+                currentColor = Color.Lerp(startColor, endColor, t);
+                selectedColor = currentColor;
+                listCadre[selectedCard * 2].color = selectedColor;
+                listCadre[selectedCard * 2 + 1].color = selectedColor;
+                yield return null;
+            }
+            counter++;
+            Color temp = startColor;
+            selectedColor = endColor;
+            listCadre[selectedCard * 2].color = selectedColor;
+            listCadre[selectedCard * 2 + 1].color = selectedColor;
+            endColor = temp;
+            timeElapsed = 0f;
+        }
+        yield break;
+    }
+
 }
